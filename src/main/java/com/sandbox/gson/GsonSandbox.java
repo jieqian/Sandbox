02829ace1d7993f07f20638eb1fc7cc6cbac32cd
qianjie <qianjie@ppcredit.com>
2017-10-26 15:04:22 +0800

    test

--- src/main/java/com/sandbox/gson/GsonSandbox.java
+++ src/main/java/com/sandbox/gson/GsonSandbox.java
@@ -12,11 +12,19 @@ public class GsonSandbox {
     public static void main(String[] args) {
         Gson gson = Gsons.DEFAUTL.instance();
         Tuple2<String,String> tuple2 = new Tuple2<>("k","v");
+
+        GsonSandbox gsonSandbox = new GsonSandbox();
+        gsonSandbox.test(gson,tuple2);
+
         String jsonStr = gson.toJson(tuple2);
         System.out.println(jsonStr);
 
         Tuple2<String,String> deserializedTuple2 = gson.fromJson(jsonStr,new TypeToken<Tuple2<String,String>>(){}.getType());
         System.out.println(deserializedTuple2._1());
         System.out.println(deserializedTuple2._2());
+    }
+
+    public void test(Gson gson, Object object){
+        System.out.println(gson.toJson(object));
     }
 }
