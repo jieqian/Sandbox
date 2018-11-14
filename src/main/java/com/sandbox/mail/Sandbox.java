f669d4133f3f107da0aaeaef652094b434b32f9a
钱杰 <jie.qian@mobilemd.cn>
2018-11-14 14:51:37 +0800

    add send email via QQ's SMTP

---
+++ src/main/java/com/sandbox/mail/Sandbox.java
@@ -0,0 +1,52 @@
+package com.sandbox.mail;
+
+import com.sun.mail.util.MailSSLSocketFactory;
+
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.security.GeneralSecurityException;
+import java.util.Properties;
+
+public class Sandbox {
+    public static void main(String[] args) throws Exception {
+        Properties prop = new Properties();
+        prop.put("mail.transport.protocol", "smtp");
+        prop.put("mail.smtp.auth", true);
+        prop.put("mail.smtp.host", "smtp.exmail.qq.com");
+        prop.put("mail.smtp.port", "465");
+        MailSSLSocketFactory sf = new MailSSLSocketFactory();
+        sf.setTrustAllHosts(true);
+        prop.put("mail.smtp.ssl.enable", "true");
+        prop.put("mail.smtp.ssl.socketFactory", sf);
+
+
+        // 创建session
+        Session session = Session.getInstance(prop);
+        // 通过session得到transport对象
+        Transport ts = session.getTransport();
+        // 连接邮件服务器：邮箱类型，帐号，授权码代替密码（更安全）
+        // 第三个参数为授权码，具体参考https://service.mail.qq.com/cgi-bin/help?subtype=1&&id=28&&no=1001256
+        ts.connect("smtp.exmail.qq.com","jie.qian@mobilemd.cn", "RY8HmNVjeNQXbKyp");
+        // 创建邮件
+        Message message = createSimpleMail(session);
+        // 发送邮件
+        ts.sendMessage(message, message.getAllRecipients());
+        ts.close();
+    }
+
+    public static MimeMessage createSimpleMail(Session session)
+            throws Exception {
+        // 创建邮件对象
+        MimeMessage message = new MimeMessage(session);
+        // 指明邮件的发件人
+        message.setFrom(new InternetAddress("jie.qian@mobilemd.cn"));
+        // 指明邮件的收件人，现在发件人和收件人是一样的，那就是自己给自己发
+        message.setRecipient(Message.RecipientType.TO, new InternetAddress("vash_ace@163.com"));
+        // 邮件的标题
+        message.setSubject("JavaMail测试");
+        // 邮件的文本内容
+        message.setContent("JavaMail发送邮件成功！", "text/html;charset=UTF-8");
+        // 返回创建好的邮件对象
+        return message;
+    }
+}
