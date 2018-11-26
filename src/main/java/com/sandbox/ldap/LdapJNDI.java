6ac272e51606a4decfa398a9d749ab43e84c5bdc
钱杰 <jie.qian@mobilemd.cn>
2018-11-26 20:50:51 +0800

    add LDAP JNDI authentication

--- src/main/java/com/sandbox/ldap/LdapJNDI.java
+++ src/main/java/com/sandbox/ldap/LdapJNDI.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.Hashtable;
 import javax.naming.*;
 import javax.naming.directory.*;
+import javax.naming.ldap.Control;
+import javax.naming.ldap.InitialLdapContext;
+import javax.naming.ldap.LdapContext;
 
 public class LdapJNDI {
 
@@ -12,54 +15,27 @@ public class LdapJNDI {
 //    LdapConfiguration ldapConfiguration;
 
     public void JNDILookup() {
-        String rootFilter = "o=cvte.com,o=isp";
-//        String filter = "(&(smart-type=E1)(smart-status=1))";
-        String filter = "(&(smart-type=E1)(uid=00012047))";
-        String username = "uid=USER_NAME,ou=Authorization,ou=People,o=cc.com,o=isp";//xxx为申请的对接账户
-        String password = "PASSW";
-
         Hashtable env = new Hashtable();
         env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");//设置连接LDAP的实现工厂
-        env.put(Context.PROVIDER_URL, "ldap://192.168.1.2:389");// 指定LDAP服务器的主机名和端口号
+        env.put(Context.PROVIDER_URL, "ldap://192.168.1.2:389/OU=Taimei,DC=taimei,DC=com");// 指定LDAP服务器的主机名和端口号
         env.put(Context.SECURITY_AUTHENTICATION, "simple");//给环境提供认证方法,有SIMPLE、SSL/TLS和SASL
+        //指定的LDAP账号的用户名(CN+BASEDN)和密码
+//        env.put(Context.SECURITY_PRINCIPAL, "CN=钱杰,OU=技术中心,OU=Taimei,DC=taimei,DC=com");//指定进入的目录识别名DN
+//        env.put(Context.SECURITY_CREDENTIALS, "Jq12345"); //进入的目录密码
+        //通用用户名和密码
         env.put(Context.SECURITY_PRINCIPAL, "taimei@taimei.com");//指定进入的目录识别名DN
         env.put(Context.SECURITY_CREDENTIALS, "Jiaxing@123"); //进入的目录密码
-//        env.put("filter",filter);
+
         DirContext ctx = null;
 
         try {
             // 得到初始目录环境的一个引用
             ctx = new InitialDirContext(env);
 
-            //The search base entry 'uid=00012047,ou=Internal,ou=People,o=cvte.com,o=isp' does not exist]; remaining name 'uid=00012047,ou=Internal'
-//            Attributes attrs = ctx.getAttributes("uid=00012047,ou=Internal,ou=People");//获取到一个人员，
-
             SearchControls constraints = new SearchControls();
             constraints.setSearchScope(SearchControls.SUBTREE_SCOPE);
-            NamingEnumeration<SearchResult> namingEnumeration = ctx.search("","cn=钱杰,ou=技术中心,ou=Taimei,dc=taimei,dc=com",constraints);
-            System.out.println(namingEnumeration);
-//            Attributes attrs = ctx.getAttributes("CN=Taimei,CN=Users,DC=taimei,DC=com");
-//            NamingEnumeration en = ctx.search("CN=Taimei","CN=Users,DC=taimei,DC=com",constraints);
-//            System.out.println(en);
-
-//            NamingEnumeration bindings = ctx.listBindings("OU=技术中心,OU=Taimei,DC=taimei,DC=com");//列举 内部人员
-//            NamingEnumeration bindings = ctx.listBindings("CN=Users,DC=taimei,DC=com");//列举 内部人员
-//
-//            while (bindings.hasMore()) {
-//                Binding bd = (Binding)bindings.next();
-//                LdapCtx object = (LdapCtx) bd.getObject();
-//                System.out.println(bd.getName() + ": " + object);
-//            }
-
-
-            /*根据结点的DN来查找它的所有属性, 然后再从属性中得到所有的值,注意一个属性可以有多个值*/
-//            for (NamingEnumeration ae = attrs.getAll(); ae.hasMore(); ) {
-//                //获取一个属性
-//                Attribute attr = (Attribute) ae.next();
-//                for (NamingEnumeration ve = attr.getAll(); ve.hasMore(); ) {
-//                    System.out.println(String.format("Attribute=%s,Value=%s",attr.getID(),ve.next()) );
-//                }
-//            }
+            NamingEnumeration<SearchResult> namingEnumeration = ctx.search("","sAMAccountName=jie.qian",constraints);
+            System.out.println(namingEnumeration.next());
 
         } catch (javax.naming.AuthenticationException e) {
             System.out.println("认证失败");
@@ -79,9 +55,87 @@ public class LdapJNDI {
 
     }
 
+    private final String URL = "ldap://192.168.1.2:389/";
+    private final String BASEDN = "OU=Taimei,DC=taimei,DC=com";
+    private final String FACTORY = "com.sun.jndi.ldap.LdapCtxFactory";
+    private LdapContext ctx = null;
+    private final Control[] connCtls = null;
+
+    private void LDAP_connect() {
+        Hashtable<String, String> env = new Hashtable<String, String>();
+        env.put(Context.INITIAL_CONTEXT_FACTORY, FACTORY);
+        env.put(Context.PROVIDER_URL, URL + BASEDN);
+        env.put(Context.SECURITY_AUTHENTICATION, "simple");
+
+        String root = "taimei@taimei.com";// root
+        env.put(Context.SECURITY_PRINCIPAL, root);
+        env.put(Context.SECURITY_CREDENTIALS, "Jiaxing@123");
+        // 此处若不指定用户名和密码,则自动转换为匿名登录
+        try {
+            ctx = new InitialLdapContext(env, connCtls);
+        } catch (javax.naming.AuthenticationException e) {
+            System.out.println("验证失败：" + e.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getUserDN(String uid) {
+        String userDN = "";
+        LDAP_connect();
+        try {
+            SearchControls constraints = new SearchControls();
+            constraints.setSearchScope(SearchControls.SUBTREE_SCOPE);
+            NamingEnumeration<SearchResult> en = ctx.search("", "sAMAccountName=" + uid, constraints);
+            if (en == null || !en.hasMoreElements()) {
+                System.out.println("未找到该用户");
+            }
+            // maybe more than one element
+            while (en != null && en.hasMoreElements()) {
+                Object obj = en.nextElement();
+                if (obj instanceof SearchResult) {
+                    SearchResult si = (SearchResult) obj;
+                    userDN += si.getName();
+                    userDN += "," + BASEDN;
+                } else {
+                    System.out.println(obj);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("查找用户时产生异常。");
+            e.printStackTrace();
+        }
+
+        return userDN;
+    }
+
+    public boolean authenticate(String UID, String password) {
+        boolean valide = false;
+        String userDN = getUserDN(UID);
+
+        try {
+            ctx.addToEnvironment(Context.SECURITY_PRINCIPAL, userDN);
+            ctx.addToEnvironment(Context.SECURITY_CREDENTIALS, password);
+            ctx.reconnect(connCtls);
+            System.out.println(userDN + " 验证通过");
+            valide = true;
+        } catch (AuthenticationException e) {
+            System.out.println(userDN + " 验证失败");
+            System.out.println(e.toString());
+            valide = false;
+        } catch (NamingException e) {
+            System.out.println(userDN + " 验证失败");
+            valide = false;
+        }
+
+        return valide;
+    }
+
     public static void main(String[] args) {
         LdapJNDI ldapJNDI = new LdapJNDI();
-        ldapJNDI.JNDILookup();
-
+//        ldapJNDI.JNDILookup();
+        String uid = "jie.qian";
+        String password = "Jq12345";
+        System.out.println(ldapJNDI.authenticate(uid,password));
     }
 }
